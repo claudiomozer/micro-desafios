b13e895642f9c6c0208c991139a8c04e1175ca57
= <=>
2021-10-27 20:42:40 -0300

    Implementa as conexões com os serviços remotos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DesafiosModule } from './desafios/desafios.module';
 import { PartidasModule } from './partidas/partidas.module';
 
+const connectionParams : object = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
 @Module({
-  imports: [DesafiosModule, PartidasModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      'mongodb+srv://' +
+      `${process.env.DB_USER}:` +
+      `${process.env.DB_PASS}@` +
+      `${process.env.DB_HOST}/` +
+      `${process.env.DB_NAME}?retryWrites=true&w=majority`
+      , connectionParams),
+    DesafiosModule, 
+    PartidasModule
+  ],
   controllers: [],
   providers: [],
 })
